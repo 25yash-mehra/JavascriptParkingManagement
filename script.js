e1c56dabe387a66550d4bd1456e7252cac568b54
red wolf <myash7535@gmail.com>
2024-06-11 22:33:53 +0530

    applying conditions

--- script.js
+++ script.js
@@ -14,12 +14,16 @@ document.getElementById("confirm").addEventListener("click", () => {
   } else if (veichalType.value === "") {
     alert("Please mention vehicle type");
   } else {
+
+
     const container = document.createElement("div");
     container.className = "entryContainer";
 
-
+//show bill;
+let endBill = document.createElement("h4")
+endBill.textContent = billButton
+document.getElementById("showBill").appendChild(endBill)
 // timing funcnality
-
   let date = new Date();
   let hours = date.getHours();
   let minutes = date.getMinutes().toString()
@@ -54,29 +58,32 @@ countTag.textContent = val++;
     let billButton = document.createElement("button");
     billButton.textContent = "Bill";
     billButton.className = "billButton";
-    billButton.addEventListener("click", () => {
-      container.remove();
-      });
-      container.appendChild(billButton);
-      
-      // Append entry container to the main container
-      entries.appendChild(container);
-      billButton.addEventListener( "click",()=>{
-      let date = new Date();
-      let hours = date.getHours();
-      let minutes = date.getMinutes().toString()
-      
-      let ampm = hours >= 12 ? 'PM' : 'AM';
-      hours = hours % 12;
-      hours = hours ? hours : 12; 
-      hours = hours.toString()
-      let currentTime = `${hours}:${minutes} ${ampm}`;
-      console.log(currentTime);
-      if(veichalType){
-
-      }
-   
+    container.appendChild(billButton);
+    entries.appendChild(container);
+    // delet button
+    let delButton = document.createElement("button")
+    delButton.textContent = "delet"
+    delButton.className = "delButton"
+    container.appendChild(delButton)
+    entries.appendChild(container)
+//delet items
+  delButton.addEventListener("click",()=>{
+  billButton.remove()
+  veichalTag.remove()
+  ownerTag.remove()
+  countTag.remove()
+  delButton.remove()
+})
+    billButton.addEventListener( "click",()=>{
+      let exitTime = new Date()
+      let setDate = Math.floor((exitTime - date)/ 60000)
+      document.getElementById("showTime").innerHTML = `${setDate} min`
+      console.log(setDate);
+      billButton.remove()
     })
+      
+  
+
       }
       });
       
